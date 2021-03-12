bac0916feb233148260a79f2b20bb50a7a3fcf4c
WENJIE <wenjie.wu@yahoo.com>
2021-03-12 16:43:38 +0800

    配置中心

---
+++ cloud-config-client3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,22 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/config")
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping(value = "/configInfo")
+    public CommonResult<?> getConfigInfo() {
+        return new CommonResult<>(200, null, configInfo);
+    }
+}
